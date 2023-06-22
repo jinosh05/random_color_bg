13fab64bcc7ad66e1a581a4078831384d8f0beb2
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-06-22 21:07:56 +0530

    Hint Text Created

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -10,8 +10,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ValueNotifier<Color> colorNotifier = ValueNotifier<Color>(Colors.white);
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final double height = MediaQuery.sizeOf(context).height;
+    final fontSize = height * 0.012;
+    const blurRadiusCount = 3;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text(
+          "Rabdom Colors",
+        ),
+      ),
+      body: ValueListenableBuilder<Color>(
+        valueListenable: colorNotifier,
+        builder: (context, Color value, child) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "Hello there",
+                ),
+              ),
+              Center(
+                child: Text(
+                  "\nClick on the button to toggle colors",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: fontSize,
+                    shadows: [
+                      for (double i = 1; i <= blurRadiusCount; i++)
+                        Shadow(
+                          color: Colors.purple,
+                          blurRadius: blurRadiusCount * i,
+                        )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
