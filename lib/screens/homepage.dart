825d910036528417763f3cc03aeb8acfba4c2963
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-06-22 20:33:21 +0530

    Main and Home Widgets created

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+/// Widget Where the UI is going to be presented
+class HomePage extends StatefulWidget {
+  /// Empty Constructor
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
